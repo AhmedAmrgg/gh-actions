934316d276e01fec417eea5c9fe87baea90e3a5f
AhmedAmrgg <166300085+AhmedAmrgg@users.noreply.github.com>
2025-04-26 18:05:46 +0000

    docker

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const htp = require('http'); // خطأ: كتبت http غلط
 
-const server = http.createServer((req, res) => {
-  res.end('Hello, Docker!');
+const server = htp.creatServer((req, res) => { // خطأ: creatServer مش createServer
+  res.end('Hello, Docker!)'; // خطأ: قوس الاقتباس مش مقفول صح
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+server.listen(3000 () => { // خطأ: نسيت فاصلة قبل الكولباك
+  console.log('Server running on port 3000'); // دي السطر الوحيد الصح 😂
+};
