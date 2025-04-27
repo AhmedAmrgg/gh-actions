6f195e8f2e42c88862ac9277cb947f7b98e7e290
AhmedAmrgg <166300085+AhmedAmrgg@users.noreply.github.com>
2025-04-27 10:46:04 +0000

    docker

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const httpp = require('http'); // غلط ف كتابة http
+const http = require('http');
 
-const fs = require('fs-extraa'); // غلط: موديول مش موجود اصلا
+const server = http.createServer((req, res) => {
+  res.end('Hello, Docker!');
+});
 
-const server = httpp.createServr((req, res) => { // غلط: createServr مش createServer
-res.end("Hello World); // غلط: double quotes مفتوحة ومفيش قفل
-
-if(req.url = "/test") { // غلط: عامل = بدل ==
- console.log('Test path'); 
-} else { 
- console.log('Other path' 
-} // غلط: مفيش قوس قافل للـ console.log
-
-server.listen(3000 () => { // غلط: مفيش فاصلة بعد 3000
- console.log("Server is running on port 3000")
- // نسيت قوس النهاية
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
